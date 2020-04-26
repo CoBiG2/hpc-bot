26f0db4b6485fb27bd9c3a48652e3902ff4c537a
Kronopt <Kronopt@users.noreply.github.com>
2020-04-26 20:03:09 +0100

    Run shell commands
    Commands:
    - Generic function to run shell commands (run_shell_cmd)
    - Applied run_shell_cmd to "home" command
    - do stuff before command invocation (cog_before_invoke)
    - do stuff when an error is raised inside the commands cog (cog_command_error)
    
    Bot:
    - Check if bot in only on a single server
    - Check if a specific bot text channel exists
    - more logging

--- cogs/commands.py
+++ cogs/commands.py
@@ -7,25 +7,27 @@ Commands Cog - bot commands
 
 import asyncio
 import logging
+import signal
 import socket
 from discord.ext import commands
+import checks
 
 
 class Commands(commands.Cog):
 
-    def __init__(self):
+    def __init__(self, bot):
         super().__init__()
+        self.bot = bot
         self.server_name = socket.gethostname()
         self.logger = logging.getLogger(__name__)
 
-    @commands.command()
-    async def test(self, ctx):
-        """
-        This is a testing command
-        Some test text
-        another line of test
-        """
-        await ctx.send('tested')
+    async def cog_before_invoke(self, ctx):
+        """Called before each command invocation"""
+        self.logger.info("calling command: {}".format(ctx.command.name))
+
+    async def cog_command_error(self, ctx, error):
+        """Called when an error is raised inside this cog"""
+        self.logger.error(error)
 
     @commands.command()
     async def name(self, ctx):
@@ -41,33 +43,91 @@ class Commands(commands.Cog):
         Server status
         CPU, RAM and total disk usage
         """
-        pass
+        await ctx.send(f'{self.server_name} status: OK')
         # TODO uptime + cpu ($ uptime)
         # TODO ram + swap usage ($ free -gh)
+        # TODO send to bot_text_channel
+        # TODO checks.can_write_to_bot_text_channel
+        # TODO embed
+        # TODO:
+        #   - see if there's a "status" message from this server/machine (if not create one)
+        #   - edit that message with the output of the command as it is being read
 
     @commands.command()
+    @commands.check(checks.can_write_to_bot_text_channel())
     async def home(self, ctx):
         """
         Space usage of home folders
-        Total space occupied by each user's home folder
+        Total space occupied by each user's home folder on each server
         """
         command = 'sudo du -sh /home/*'
-        process = await asyncio.create_subprocess_shell(
-            command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        await self.run_shell_cmd(ctx, command, self.handle_home)
 
-        while not process.stdout.at_eof():  # if command isn't done outputting to stdout
-            line_read = await process.stdout.readline()
-            line = line_read.decode('utf-8').rstrip()
-            if line:
-                await ctx.send(line)
+    async def handle_home(self, ctx, line):
+        """
+        Handles home command output
 
-        while not process.stderr.at_eof():  # if command isn't done outputting to stderr
-            line_read = await process.stderr.readline()
-            line = line_read.decode('utf-8').rstrip()
-            if line:
-                await ctx.send(line)
-
+        Parameters
+        ----------
+        ctx: Context
+            context
+        line: str
+            line of command output
+        """
+        await self.bot.bot_text_channel.send(line)
         # TODO embed
         # TODO:
-        #   - see if there's a "home" message from this server (if not create one)
+        #   - see if there's a "home" message from this server/machine (if not create one)
         #   - edit that message with the output of the command as it is being read
+
+    async def run_shell_cmd(self, ctx, cmd, handle_output_line):
+        """
+        Runs shell command 'cmd' on the local machine
+
+        Parameters
+        ----------
+        ctx: Context
+            context
+        cmd: str
+            command to be executed
+        handle_output_line: function
+            function to handle each line of output produced by the command
+        """
+        async with ctx.channel.typing():
+            process = await asyncio.create_subprocess_shell(
+                cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+
+            # read each line of stdout
+            while not process.stdout.at_eof():  # if command isn't done outputting to stdout
+                line_read = await process.stdout.readline()
+                line = line_read.decode('utf-8').rstrip()
+                if line:
+                    await handle_output_line(ctx, line)
+
+            # no error while running the command
+            if process.returncode == 0:
+                await ctx.send("Command `{}` finished running successfully".format(ctx.command.name))
+                # TODO every server will send this message...
+
+            # errors
+            else:
+                _, stderr = await process.communicate()  # wait for command to terminate and finish reading stderr
+
+                # signal terminated
+                if process.returncode < 0:
+                    signal_code = abs(process.returncode)
+                    await ctx.send("Error: command `{}` terminated by signal `{} ({})`".format(
+                        ctx.command.name,
+                        signal_code,
+                        signal.Signals(signal_code).name))
+                    # TODO every server will send this message...
+
+                # error return code
+                else:
+                    await ctx.send("Error: command `{}` terminated with error code `{}`".format(
+                        ctx.command.name,
+                        process.returncode))
+                    # TODO every server will send this message...
+                    # TODO find a way to get return code name with python...
+
+                self.logger.error(stderr.decode('utf-8').rstrip())
--- hpc_bot.py
+++ hpc_bot.py
@@ -15,6 +15,11 @@ from discord.ext import commands
 import cogs
 
 
+# TODO config file (bot text channel, server name, logfile)
+BOT_TEXT_CHANNEL = 'bot-test'
+LOG_FILE = 'bot.log'
+
+
 if __name__ == '__main__':
     # command line interface stuff
     cli = argparse.ArgumentParser(description='Run hpc-bot discord Bot')
@@ -24,21 +29,38 @@ if __name__ == '__main__':
     # logging stuff
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger('discord')
-    handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='a')
+    handler = logging.FileHandler(filename=LOG_FILE, encoding='utf-8', mode='a')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
 
     # bot setup
     bot = commands.Bot(command_prefix=commands.when_mentioned)  # bot only reacts when mentioned: @<bot_name> <command>
-    bot.add_cog(cogs.Commands())
+    bot.add_cog(cogs.Commands(bot))
     bot.help_command = commands.MinimalHelpCommand()
 
     # do some stuff when bot is ready
     @bot.event
     async def on_ready():
-        await bot.change_presence(activity=discord.Game("Type @{} help".format(bot.user.name)))
+        await bot.change_presence(activity=discord.Game('Type @{} help'.format(bot.user.name)))
         logger.info('Logged in as: %s, (id: %s)' % (bot.user.name, bot.user.id))
-        logger.info('Bot is ready')
+
+        # check how many servers this bot is connected to (should only be one)
+        if len(bot.guilds) > 1:
+            logger.error('Bot can only be active on a single server')
+            await bot.logout()
+
+        # check if bot-specific text channel exists
+        else:
+            bot.bot_text_channel = ''
+            for text_channel in bot.guilds[0].text_channels:
+                if text_channel.name == BOT_TEXT_CHANNEL:
+                    bot.bot_text_channel = text_channel
+                    break
+            if not bot.bot_text_channel:
+                logger.error('No text channel named "{}" exists on the server'.format(BOT_TEXT_CHANNEL))
+                await bot.logout()
+            else:
+                logger.info('Bot is ready')
 
     # start bot
     logger.info('Starting bot')
@@ -46,3 +68,5 @@ if __name__ == '__main__':
     logger.info('bot shutdown complete')
 
     # TODO help command (based on commands.MinimalHelpCommand)
+    # TODO what to do on bot crash
+    # TODO what to do on machine restart (auto-run)
