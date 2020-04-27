7f30dbd8a82bb0ec4ed4cb6d227aae409e963319
Kronopt <Kronopt@users.noreply.github.com>
2020-04-27 01:18:31 +0100

    status command

--- cogs/commands.py
+++ cogs/commands.py
@@ -9,6 +9,8 @@ import asyncio
 import logging
 import signal
 import socket
+import discord
+from functools import partial
 from discord.ext import commands
 import checks
 
@@ -23,7 +25,9 @@ class Commands(commands.Cog):
 
     async def cog_before_invoke(self, ctx):
         """Called before each command invocation"""
-        self.logger.info("calling command: {}".format(ctx.command.name))
+        user = ctx.author.nick if isinstance(ctx.author, discord.Member) else ctx.author.name
+        self.logger.info("calling command: {}, by user: {}, from channel: {}".format(
+            ctx.command.name, user, ctx.channel))
 
     async def cog_command_error(self, ctx, error):
         """Called when an error is raised inside this cog"""
@@ -38,16 +42,54 @@ class Commands(commands.Cog):
         await ctx.send(f'server name: {self.server_name}')
 
     @commands.command()
+    @commands.check(checks.can_write_to_bot_text_channel())
     async def status(self, ctx):
         """
         Server status
         CPU, RAM and total disk usage
         """
-        await ctx.send(f'{self.server_name} status: OK')
-        # TODO uptime + cpu ($ uptime)
-        # TODO ram + swap usage ($ free -gh)
-        # TODO send to bot_text_channel
-        # TODO checks.can_write_to_bot_text_channel
+        command_cpu_and_uptime = 'uptime'
+        command_ram_and_swap = 'free -gh'
+        command_disk_usage = 'df -h --total | tail -n 1'
+        await self.run_shell_cmd(ctx, command_cpu_and_uptime, partial(self.handle_status, cmd_output='cpu_and_time'))
+        await self.run_shell_cmd(ctx, command_ram_and_swap, partial(self.handle_status, cmd_output='ram_and_swap'))
+        await self.run_shell_cmd(ctx, command_disk_usage, partial(self.handle_status, cmd_output='disk_usage'))
+
+    async def handle_status(self, ctx, line, cmd_output=''):
+        """
+        Handles status command output
+
+        Parameters
+        ----------
+        ctx: Context
+            context
+        line: str
+            line of command output
+        cmd_output: str
+            what command output to expect
+        """
+        if cmd_output == 'cpu_and_time':
+            uptime = line[line.find('up ')+3:line.find('user')].rsplit(',', maxsplit=1)[0]
+            cpu_usage = line[line.rfind('load average: ')+14:]  # 1, 5 and 15 minutes average
+            await self.bot.bot_text_channel.send(f'uptime: {uptime}\ncpu usage: {cpu_usage}')
+
+        elif cmd_output == 'ram_and_swap':
+            line_contents = line.split()
+            # ignores first line, which only contains column headers
+            if line_contents[0] == 'Mem:':
+                _, ram_total, ram_used, ram_free, _, _, ram_available = line_contents
+                await self.bot.bot_text_channel.send(
+                    f'ram:\ntotal {ram_total}\nused {ram_used}\nfree {ram_free}\navailable {ram_available}')
+            elif line_contents[0] == 'Swap:':
+                _, swap_total, swap_used, swap_free = line_contents
+                await self.bot.bot_text_channel.send(f'swap:\ntotal {swap_total}\nused {swap_used}\nfree {swap_free}')
+
+        elif cmd_output == 'disk_usage':
+            _, disk_size, disk_used, disk_available, disk_use_percentage, _ = line.split()
+            await self.bot.bot_text_channel.send(
+                f'disk:\nsize {disk_size}\nused {disk_used}\navailable {disk_available}\n'
+                f'used percentage {disk_use_percentage}')
+
         # TODO embed
         # TODO:
         #   - see if there's a "status" message from this server/machine (if not create one)
@@ -75,6 +117,7 @@ class Commands(commands.Cog):
             line of command output
         """
         await self.bot.bot_text_channel.send(line)
+
         # TODO embed
         # TODO:
         #   - see if there's a "home" message from this server/machine (if not create one)
