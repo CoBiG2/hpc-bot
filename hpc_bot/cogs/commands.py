80dcded54e1303b0bd1dcfa89180225c988e4b52
Kronopt <Kronopt@users.noreply.github.com>
2020-04-30 00:21:46 +0100

    plus 1 second

--- hpc_bot/cogs/commands.py
+++ hpc_bot/cogs/commands.py
@@ -175,7 +175,7 @@ class Commands(commands.Cog):
                 line_read = await process.stdout.readline()
                 line = line_read.decode('utf-8').rstrip()
                 if line:
-                    await asyncio.sleep(random.random())  # handles rate limit when using multiple servers
+                    await asyncio.sleep(random.random() + 1)  # handles rate limit when using multiple servers
                     await handle_output_line(ctx, line)
 
             # no error while running the command
