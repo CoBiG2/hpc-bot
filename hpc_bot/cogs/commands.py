ba54781210985d9f1f11a1b332ab3564b0c39ae3
Francisco Pina Martins <f.pinamartins@gmail.com>
2020-05-06 22:38:19 +0100

    Changes a pair of icons

--- hpc_bot/cogs/commands.py
+++ hpc_bot/cogs/commands.py
@@ -136,7 +136,7 @@ class Commands(commands.Cog):
                 _, ram_total, ram_used, ram_free, _, ram_cache, ram_available = line_contents
                 await kwargs.get('message_sent').edit(
                     embed=kwargs.get('embed').add_field(
-                        name='RAM 💾',
+                        name='RAM ',
                         value=f'total : {ram_total}\n'
                               f'used : {ram_used}\n'
                               f'free : {ram_free}\n'
@@ -157,7 +157,7 @@ class Commands(commands.Cog):
             _, disk_size, disk_used, disk_available, disk_use_percentage, _ = line.split()
             await kwargs.get('message_sent').edit(
                 embed=kwargs.get('embed').add_field(
-                    name='DISK 💿',
+                    name='STORAGE ',
                     value=f'size : {disk_size}\n'
                           f'available : {disk_used}\n'
                           f'used : {disk_available}\n'
