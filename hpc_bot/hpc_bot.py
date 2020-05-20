95e8b79cc363661b7fab07fa39f888dacc0abe52
Francisco Pina Martins <f.pinamartins@gmail.com>
2020-05-20 23:28:43 +0100

    Bumps package version

--- hpc_bot/hpc_bot.py
+++ hpc_bot/hpc_bot.py
@@ -28,7 +28,7 @@ __author__ = 'Pedro HC David, https://github.com/Kronopt'
 __credits__ = ['Pedro HC David', 'Francisco Pina Martins']
 __copyright__ = 'Copyright Pedro HC David & Francisco Pina Martins 2020'
 __license__ = 'GPLv3'
-__version__ = '0.1.0a1'
+__version__ = '0.1.1'
 __url__ = 'https://github.com/CoBiG2/hpc-bot'
 __description__ = 'A discord bot to relay HPC information.'
 
